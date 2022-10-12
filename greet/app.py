6078148f5b5dc88813727fc2bc411e0fafa56183
trevorhudson <trevor.c.hudson@gmail.com>
2022-10-12 12:26:48 -0700

    finished greet

--- greet/app.py
+++ greet/app.py
@@ -4,16 +4,20 @@ app = Flask(__name__)
 
 @app.get('/welcome')
 def welcome_message():
+    '''prints welcome message on screen'''
     html = "<body><h1> welcome </body></h1>"
     return html
 
 @app.get('/welcome/home')
 def welcome_home_message():
+   '''prints welcome home message on screen'''
+
     html = "<body><h1> welcome home </body></h1>"
     return html
 
 @app.get('/welcome/back')
 def welcome_back_message():
+    '''prints welcome back message on screen'''
     html = "<body><h1> welcome back </body></h1>"
     return html
 
