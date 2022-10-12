b27c6de34131cb51cc5cbe76600beed7d2791831
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-12 15:21:28 -0400

    Get calc app working, need to fix unittest

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,42 @@
-# Put your app in here.
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.get('/add')
+def add():
+    """Returns the sum of a and b"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return f"<html><body><h1> The sum is {a + b}</h1></body></html>"
+
+
+@app.get('/subtract')
+def subtract():
+    """Returns the difference between a and b"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return f"<html><body><h1> The difference is {a - b}</h1></body></html>"
+
+
+@app.get('/multiply')
+def multiply():
+    """Returns the product between a and b"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return f"<html><body><h1> The product is {a * b}</h1></body></html>"
+
+
+@app.get('/divide')
+def divide():
+    """Returns the quotient between a and b"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return f"<html><body><h1> The quotient is {a / b}</h1></body></html>"
